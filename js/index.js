8efe9a551631198d8fa3a70f017a1991eadc0e8b
JacobWylie <jacobwylie@gmail.com>
2018-05-21 18:21:32 +0200

    Start building project page, run some functionality for the main page scroll effects

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 // Select needed elements and store as variables
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-menu');
+const hamburger   = document.querySelector('.hamburger'),
+      navMenu     = document.querySelector('.nav-menu'),
+      headerText  = document.querySelector('.main-header .title'),
+      headerArrow = document.querySelector('.main-header svg');
 
 // Functionality for burger menu
 function burgerMenu() {
@@ -11,3 +13,10 @@ function burgerMenu() {
 
 // Handles click functionality on burger menu
 hamburger.addEventListener('click', burgerMenu);
+
+window.addEventListener('scroll', function() {
+	// Fades the main-header text and svg arrow when user scrollbars.
+	const opacity = 1 - (this.scrollY / 500);
+    headerText.style.opacity = opacity;
+    headerArrow.style.opacity = opacity;
+});
