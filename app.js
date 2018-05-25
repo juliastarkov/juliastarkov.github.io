c31524a8885d89772cb774d7b3b28675dea7f1ea
JacobWylie <jacobwylie@gmail.com>
2018-05-26 00:19:28 +0200

    npm packages, env variable setup, and contact form mailer app

---
+++ app.js
@@ -0,0 +1,62 @@
+const express    = require('express'),
+	  app        = express(),
+	  nodemailer = require('nodemailer'),
+	  mg         = require('nodemailer-mailgun-transport'),
+	  bodyParser = require('body-parser'),
+	  dotENV     = require('dotenv').load(),
+	  auth       = {"auth": {"api_key" : process.env.API_KEY, "domain" : process.env.DOMAIN}};
+        			
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/", (req, res) => {
+	const name = req.body.name;
+	const email = req.body.email;
+	const message = req.body.message;
+
+	// create transporter object capable of sending email using the default SMTP transport
+	const transporter = nodemailer.createTransport(mg(auth));
+
+	const mailOptions = {
+		from: email,
+		name: name,
+		to: 'jstarkov8@gmail.com', 
+		subject: 'New Message From Your Portfolio Contact Page',
+		text: message
+	};
+	
+	// send mail with defined transport object
+	transporter.sendMail(mailOptions, (error, info) => {
+		if (error) {
+			// Simple log to server, add proper UI response to user at res.
+			// or add flash message to page with https://www.npmjs.com/package/connect-flash 
+	  		console.log('\nERROR: ' + error+'\n');
+	  		res.redirect('back');
+		} else {
+			// Simple log to server, add proper UI response to user at res.
+			// or add flash message to page https://www.npmjs.com/package/connect-flash 
+	    	console.log('\nRESPONSE SENT: ' + info.response+'\n');
+	  		res.redirect('back');
+		}
+	});
+
+});
+
+// Set which port your app will run on: PORT = open port on your server
+const port = process.env.PORT;
+app.listen(port, () => {
+	console.log(`App is running on port: ${port}`);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/index.js
+++ js/index.js
@@ -1,39 +1,39 @@
 // Select needed elements and store as variables
-const hamburger   = document.querySelector('.hamburger'),
-      navMenu     = document.querySelector('.nav-menu'),
-      headerText  = document.querySelector('#main-header .title'),
-      headerArrow = document.querySelector('#main-header svg'),
-      navBtns     = [...document.querySelectorAll('.nav-links a')];
+const hamburger         = document.querySelector('.hamburger'),
+      navMenu           = document.querySelector('.nav-menu'),
+      headerText        = document.querySelector('#main-header .title'),
+      headerArrow       = document.querySelector('#main-header svg'),
+      navBtns           = [...document.querySelectorAll('.nav-links a')],
+      // Get each content section's height for smooth scroll functionality
+      headerHeight      = document.getElementById('main-header').offsetHeight,
+      workHeight        = document.getElementById('work').offsetHeight,
+      aboutHeight       = document.getElementById('about').offsetHeight,
+      contactHeight     = document.getElementById('contact').offsetHeight;
 
 // Functionality for burger menu
 function burgerMenu() {
-	// Adds/Removes class from burger for css animation
 	this.classList.toggle('is-active');
 	navMenu.classList.toggle('open');
 }
+hamburger.addEventListener('click', burgerMenu);
 
 // Smooth Scrolling Nav Buttons
 // https://gist.github.com/andjosh/6764939
-function scrollTo(element, to, duration= 500) {
+function scrollTo(element, to, duration) {
 	const start     = element.scrollTop,
 		  change    = to - start,
 		  increment = 20;
 	let currentTime = 0;
-
 	const animateScroll = (() => {
     	currentTime += increment;
-    
     	const val = Math.easeInOutQuad(currentTime, start, change, duration);
-    
     	element.scrollTop = val;
-
     	if (currentTime < duration) {
     		setTimeout(animateScroll, increment);
   		}
 	});
 	animateScroll();
 };
-
 Math.easeInOutQuad = function (t, b, c, d) {
 	t /= d/2;
 	if (t < 1) return c/2*t*t + b;
@@ -41,60 +41,54 @@ Math.easeInOutQuad = function (t, b, c, d) {
 	return -c/2 * (t*(t-2) - 1) + b;
 };
 
+// Removes 'active' class from all nav-link and adds to clicked link
 function changeActiveNav(thisNav) {
 	navBtns.forEach(nav => {
 		nav.classList.remove('active');
 	})
-	// nav - link styles as 'active'
 	thisNav.classList.add('active');
 }
 
 // For each nav link, find the corresponding content's position on page and assign to node object
 // 'Smooth Scroll' to position on page when nav-link is clicked
-navBtns.forEach(nav => {
-	const id    = nav.textContent.toLowerCase();
-	// Assign corresponding element to node
+navBtns.forEach((nav, i) => {
+	const id = nav.textContent.toLowerCase();
+	// Assign corresponding element to node object
 	nav.el = document.getElementById(id);
-	// Find positon of corresponding content on page and assign to node	  
+	// Find positon of corresponding content on page and assign to node object	  
 	nav.position = (nav.el.offsetTop - document.body.scrollTop);
 	nav.addEventListener('click', function(e) {
 		e.preventDefault();
-		// Remove active class from all navs
+		// Remove active class from all nav-links and assign to clicked nav-link
 		changeActiveNav(this);
 		// Scroll to content
-		scrollTo(document.documentElement, nav.position);
+		scrollTo(document.documentElement, nav.position, 750);
 	})
 })
 
-// Handles click functionality on burger menu
-hamburger.addEventListener('click', burgerMenu);
-
-// SUPER HACKY SELECTING FOR NAVBAR SCROLLING ACTIVE CLASSES
-const nav1 = navBtns[0];
-const nav2 = navBtns[1];
-const nav3 = navBtns[2];
-const nav4 = navBtns[3];
-const header = document.getElementById('main-header').offsetHeight;
-const work = document.getElementById('work').offsetHeight;
-const about = document.getElementById('about').offsetHeight;
-const contact = document.getElementById('contact').offsetHeight;
-
+//////////////////////////////////////////////////////////
+// REFACTOR THE NAVBAR SCROLL 'ACTIVE CLASS' CODE BELOW
+// IT WORKS BUT VERY UGLY...
+//////////////////////////////////////////////////////////
+// Handles scroll functionality
 window.addEventListener('scroll', function() {
-	// Fades the main-header text and svg arrow when user scrollbars.
+	// Fades the main-header text and svg arrow
 	const opacity = 1 - (this.scrollY / 500);
     headerText.style.opacity = opacity;
     headerArrow.style.opacity = opacity;
 
     // VERY HACKY WAY TO HANDLE NAVBAR SCROLLING ACTIVE CLASSES 
     // MAKE IT PRETTY!!
-    if(document.documentElement.scrollTop <= header - 70) {
-    	changeActiveNav(nav1);
-    } else if(document.documentElement.scrollTop <= (header + work - 70)) {
-    	changeActiveNav(nav2);
-    } else if(document.documentElement.scrollTop <= (header + work + about - 70)) {
-    	changeActiveNav(nav3);
+    // Follow pattern to add another section
+    // '- 70' allows the scroll to go past the nav height
+    if(document.documentElement.scrollTop <= headerHeight - 70) {
+    	changeActiveNav(navBtns[0]);
+    } else if(document.documentElement.scrollTop <= (headerHeight + workHeight - 70)) {
+    	changeActiveNav(navBtns[1]);
+    } else if(document.documentElement.scrollTop <= (headerHeight + workHeight + aboutHeight - 70)) {
+    	changeActiveNav(navBtns[2]);
     } else {
-    	changeActiveNav(nav4);
+    	changeActiveNav(navBtns[3]);
     }
 });
 
