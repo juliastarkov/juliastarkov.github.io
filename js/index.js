47965f3612bd447b131682480c98a52ec66aeedb
JacobWylie <jacobwylie@gmail.com>
2018-08-05 18:05:02 +0200

    style fixes and initial carousel added

--- js/index.js
+++ js/index.js
@@ -125,6 +125,35 @@ if(document.querySelector('#main-header')) {
 	});
 }
 
+// Simple Slider Init
+var slider = new simpleSlider('.simple-slider');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
